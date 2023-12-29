4615fc84b449dd9429f675bc96517e6cf9d53b2a
Restyled.io <commits@restyled.io>
2023-12-29 19:25:26 +0000

    Restyled by black

--- launch/map_server.launch.py
+++ launch/map_server.launch.py
@@ -7,22 +7,20 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
     config = os.path.join(
-        get_package_share_directory('navigation_lite'),
-        'config',
-        'sensors.yaml'
+        get_package_share_directory("navigation_lite"), "config", "sensors.yaml"
     )
 
     node = Node(
-        package='navigation_lite',
-        name='map_server',
-        executable='map_server',
+        package="navigation_lite",
+        name="map_server",
+        executable="map_server",
         output="screen",
         emulate_tty=True,
         parameters=[
-            {'map_frame': 'map'},
-            {'map_topic': 'nav_lite/map'},
-            {'pointcloud_topic': 'sensors/pointcloud'}
-        ]
+            {"map_frame": "map"},
+            {"map_topic": "nav_lite/map"},
+            {"pointcloud_topic": "sensors/pointcloud"},
+        ],
     )
     ld.add_action(node)
     return ld
